7a1585f4069e4932bf6fa2970d3657ead7749666
swethamothe <swethamothe@gmail.com>
2022-03-04 00:27:30 -0800

    .

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-	region  = us-east-2
+	region  = var.region
 }
     
 resource "aws_instance" "main" {
-	ami           = ami-0b614a5d911900a9b
-	instance_type = t2.micro
+	ami           = var.ami
+	instance_type = var.instance_type
 }
 
 
